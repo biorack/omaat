cbbf8d6c1ed8272681c9a19afe67207d08b3210e
Ben Bowen <ben.bowen@gmail.com>
2017-02-01 16:40:31 -0800

    added first tests and .travis.yaml

---
+++ tests/test_all.py
@@ -0,0 +1,6 @@
+import omaat_lib as omaat
+
+def test_fileselector():
+    if "openMSIsession" not in locals():
+        openMSIsession=omaat.OpenMSIsession()
+        openMSIsession.imageLoader_with_dialogs() #once loaded the image will be stored in the "img" variable
