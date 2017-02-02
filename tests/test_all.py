14d85dd8a1b54a34fda151ce9c5e3ffd87019889
Ben Bowen <ben.bowen@gmail.com>
2017-02-02 10:33:03 -0800

    slightly more complex test

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,4 +1,29 @@
-import omaat_lib as omaat
+# import omaat_lib as omaat
+
+
+import posixpath as path
+import numpy as np
+from scipy import interpolate
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import getpass
+import json
+import requests
+import IPython.display
+import ast
+# import abc
+import sys
+import pandas as pd
+import datetime
+import time
+import pickle
+# import inspect
+# from future.utils import with_metaclass
+
+try:
+    import ipywidgets
+except ImportError:
+    import IPython.html.widgets as ipywidgets
 
 def test_simplest_test():
 	pass
