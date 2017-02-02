01250b9f226bc99c06d490951526b79cf01c2cc0
Ben Bowen <ben.bowen@gmail.com>
2017-02-02 06:17:11 -0800

    added setup.py with necessary packages

---
+++ setup.py
@@ -0,0 +1,54 @@
+"""Setup script for omaat package.
+"""
+DISTNAME = 'omaat'
+DESCRIPTION = 'OpenMSI Arrayed Analysis Tools'
+LONG_DESCRIPTION = open('README.rst', 'rb').read().decode('utf-8')
+MAINTAINER = 'Ben Bowen'
+MAINTAINER_EMAIL = 'bpbowen@lbl.gov'
+URL = 'http://github.com/biorack/omaat'
+LICENSE = 'MIT'
+REQUIRES = ["numpy", "pandas", "dataset", "__future__", "buitins", "posixpath"
+"scipy", "matplotlib", "getpass", "json", "requests", "ipython", "ast", "abc", "sys",
+"datetime", "time", "pickle", "inspect", "future", "ipywidgets"]
+
+CLASSIFIERS = """\
+Development Status :: 2 - Pre-Alpha
+Intended Audience :: Developers
+Intended Audience :: Science/Research
+Operating System :: OS Independent
+Programming Language :: Python
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3.4
+Topic :: Scientific/Engineering
+Topic :: Software Development
+"""
+
+
+#ADD Versioning
+# with open('omaat/__init__.py') as fid:
+#     for line in fid:
+#         if line.startswith('__version__'):
+#             version = line.strip().split()[-1][1:-1]
+#             break
+
+
+if __name__ == "__main__":
+
+    setup(
+        name=DISTNAME,
+        # version=version,
+        maintainer=MAINTAINER,
+        maintainer_email=MAINTAINER_EMAIL,
+        url=URL,
+        download_url=URL,
+        # license=LICENSE,
+        platforms=["Any"],
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        classifiers=list(filter(None, CLASSIFIERS.split('\n'))),
+        packages=find_packages(exclude=['doc']),
+        include_package_data=True,
+        zip_safe=False,  # the package can run out of an .egg file
+        install_requires=REQUIRES,
+        requires=REQUIRES,
+     )
